4bf9b54ddc63ac646f2388118652098724c0723c
EkaitzB <ekaitz.blancano@elorrieta-errekamari.com>
2025-01-15 17:05:59 +0100

    Conect client-server

--- src/controller/App.java
+++ src/controller/App.java
@@ -1,10 +1,34 @@
 package controller;
 
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import model.Users;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			Socket socket = new Socket("localhost", 23456);
+			System.out.println("Connected to server!");
+			Scanner sc = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 
+			String txt = sc.nextLine();
+			out.println(txt);
+			
+			Users response = (Users) ois.readObject();
+			System.out.println(response.toString());
+				
+			System.out.println("Closing communication");
+			
+			} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
