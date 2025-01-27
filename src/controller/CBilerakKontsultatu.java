9700c084772e84441c0a6356d223de2b1fa73740
EkaitzB <ekaitz.blancano@elorrieta-errekamari.com>
2025-01-27 10:08:18 +0100

    bilerak atera

--- src/controller/CBilerakKontsultatu.java
+++ src/controller/CBilerakKontsultatu.java
@@ -1,15 +1,33 @@
 package controller;
 
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 
+import model.GlobalVariables;
+import model.Horarios;
+import model.MBilerakKontsultatu;
+import model.MOrdutegiaKontsultatu;
+import model.Reuniones;
 import view.VBilerakKontsultatu;
 
 public class CBilerakKontsultatu {
 	
+	private MOrdutegiaKontsultatu mOrdutegiaKontsultatu;
+	private MBilerakKontsultatu mBilerakKontsultatu;
+	
 	private VBilerakKontsultatu vBilerakKontsultatu;
 	
+	private ArrayList<Horarios> horariosList;
+	private ArrayList<Reuniones> bilerakList;
+	
 	public CBilerakKontsultatu() {
-		vBilerakKontsultatu = new VBilerakKontsultatu();
+		mOrdutegiaKontsultatu = new MOrdutegiaKontsultatu();
+		horariosList = mOrdutegiaKontsultatu.getHorariosByIrakasleId(GlobalVariables.loggedUser.getId());
+		mBilerakKontsultatu = new MBilerakKontsultatu();
+		bilerakList = mBilerakKontsultatu.bilerakByTeacher(GlobalVariables.loggedUser.getId());
+		
+		vBilerakKontsultatu = new VBilerakKontsultatu(horariosList, bilerakList);
 		vBilerakKontsultatu.setVisible(true);
 		
 		start();
--- src/view/VBilerakKontsultatu.java
+++ src/view/VBilerakKontsultatu.java
@@ -1,35 +1,87 @@
 package view;
 
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+import model.Horarios;
+import model.Reuniones;
+import model.view.TableRenderSchedule;
+import model.view.TableUtils;
 
 public class VBilerakKontsultatu extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	TableUtils tableUtils = new TableUtils();
 
+	private JButton btnAtzera;
 	private JButton btnLogout;
+	private JTable table;
+	private TableRenderSchedule tableRender = new TableRenderSchedule();
 	
-	public VBilerakKontsultatu() {
+	public VBilerakKontsultatu(ArrayList<Horarios> horariosList, ArrayList<Reuniones> bilerakList) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 662, 486);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
-		btnLogout = new JButton("btnLogout");
-		btnLogout.setBounds(10, 227, 89, 23);
-		contentPane.add(btnLogout);
+		System.out.println(horariosList.size());
+		
+
+		DefaultTableModel model = new DefaultTableModel(new Object[]{"Lunes", "Martes", "Miercoles", "Jueves", "Viernes"}, 5);
+
+		table = new JTable(model);
+		table.setEnabled(false);
+		table.setBounds(10, 110, 626, 104);
+		
+		
+		
+		JTableHeader header = table.getTableHeader();
+		header.setReorderingAllowed(false);
+		
+		table.setDefaultRenderer(Object.class, tableRender);
+		
+
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setEnabled(false);
+		scrollPane.setBounds(10, 110, 626, 104);
+		contentPane.add(scrollPane);
+		
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		btnAtzera = new JButton("btnAtzera");
+		btnAtzera.setBounds(10, 45, 89, 23);
+		contentPane.add(btnAtzera);
+		
+		btnLogout = new JButton("btnLogout");
+		btnLogout.setBounds(10, 11, 89, 23);
+		contentPane.add(btnLogout);
+		
+		tableUtils.fillTable(table, horariosList);
+		tableUtils.fillTableReuniones(table, bilerakList);
 	}
-	
+
 	public JButton getBtnLogout() {
 		return btnLogout;
+	}
+	
+	public JButton getBtnAtzera() {
+		return btnAtzera;
+	}
+	
+	public JTable getTable() {
+		return table;
 	}
 
 }
